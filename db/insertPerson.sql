24b050ff618eeadac0d6ec15720e14ef6147cc92
dcp12345678 <dcp12345678@yahoo.com>
2016-09-04 20:47:59 -0400

    added insertPerson.sql script

---
+++ db/insertPerson.sql
@@ -0,0 +1,11 @@
+INSERT INTO PERSON ( last_name
+                   , first_name
+                   , address
+                   , age
+                   , dept_id )
+            VALUES ( $1
+                   , $2
+                   , $3
+                   , $4
+                   , $5
+                   )
